931f1c3f937f75d39b824badb56812a1bc9ab5b7
Sadeepa Dhananjaya <115467947+syph3r-d@users.noreply.github.com>
2025-05-05 11:44:43 +0530

    Add dotenv configuration and update database connection; remove default.json

--- config/db.js
+++ config/db.js
@@ -1,20 +1,18 @@
-const mongoose=require('mongoose');
-const config=require('config');
+const mongoose = require("mongoose");
+const config = require("./config");
 
-
-const db = config.get('mongoURI');
+const db = config.mongoURI;
 mongoose.set("strictQuery", false);
 
-const connectDB=async()=>{
-    try{
-        await mongoose.connect(db);
-        console.log('Database Connected')
-    }
-    catch(err){
-        console.error(err.message);
-        console.log('Error connecting to the database');
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("Database Connected");
+  } catch (err) {
+    console.error(err.message);
+    console.log("Error connecting to the database");
+    process.exit(1);
+  }
+};
 
-module.exports=connectDB
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
-
 //connect Database
 connectDB();
 
